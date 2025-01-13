ebfd6c88b924159a913fa60316aae8e3e4f09221
Pranav Desai <daftylooper@pop-os.localdomain>
2025-01-13 16:34:10 +0530

    implement logger

---
+++ src/utils/logger.py
@@ -0,0 +1,42 @@
+import inspect
+from datetime import datetime
+
+# log levels - DEBUG, INFO, WARNING, ERROR, CRITICAL
+
+def log(level, message):
+    # get calling frame context
+    frame = inspect.currentframe().f_back
+    file_name = frame.f_code.co_filename
+    line_number = frame.f_lineno
+
+    function_name = frame.f_code.co_name
+    class_name = None
+    for cls in inspect.getouterframes(frame):
+        if cls.function != '<module>':
+            if 'self' in frame.f_locals:
+                class_name = frame.f_locals['self'].__class__.__name__
+            break
+
+    # build log details
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    context = f"{file_name}:{line_number}"
+    if class_name:
+        context += f" {class_name}.{function_name}"
+    else:
+        context += f" {function_name}"
+
+    log_message = f"[{formatted_time}] [{level.upper()}] [{context}] {message}"
+    print(log_message)
+
+# class ExampleClass:
+#     def example_method(self):
+#         log("info", "This is a log message from inside a method.")
+
+# def example_function():
+#     log("debug", "This is a log message from inside a function.")
+
+# if __name__ == "__main__":
+#     example_function()
+#     example_instance = ExampleClass()
+#     example_instance.example_method()
