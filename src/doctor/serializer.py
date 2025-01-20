6238863103cdba88da6bd8c21f26ac4f49fe9b66
Pranav Desai <daftylooper@pop-os.localdomain>
2025-01-20 13:21:26 +0530

    add model and view testcases for doctor

--- src/doctor/serializer.py
+++ src/doctor/serializer.py
@@ -5,3 +5,12 @@ class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = "__all__"
+        extra_kwargs = {'allow_unknown': False}
+
+    def validate(self, data):
+        unknown_fields = set(self.initial_data.keys()) - set(self.fields.keys())
+        if unknown_fields:
+            raise serializers.ValidationError(
+                f"Unknown field(s): {', '.join(unknown_fields)}"
+            )
+        return data
